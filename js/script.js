65f17d10b4529ea78d92205c4dde29870f20509f
vStaus <vegard.stausland@gmail.com>
2022-01-10 22:06:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-//oppgave 1.
+//oppgave 1
 var number = 8;
 var isLoggedIn = true;
 var myPet ="dog";
 
-//oppgave 2.
+//oppgave 2
 var myName = "vegard";
 var myLastName = "stausland";
 var myName = myName + " " + myLastName;
 console.log(myName);
 
-//oppgave 3.
+//oppgave 3
 var name = "frog";
 console.log(typeof name)
 //the type of frog is string
 
-//oppgave 4.
+//oppgave 4
+var orderHasShipped = true;
+console.log(orderHasShipped)
+//decleared and initialized
+var orderHasShipped = true;
+if(orderHasShipped === true)    {
+    console.log("The order shipped");
+}
+else {
+    console.log("The order did not ship");
+}
+//oppgave 5
